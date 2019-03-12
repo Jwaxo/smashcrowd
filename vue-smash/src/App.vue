7674a17950c9e82f93d01cb8324987e588f2e7ef
Christopher Bloom <bloomcb@gmail.com>
2019-03-12 10:53:22 -0700

    Very early vue approach

---
+++ vue-smash/src/App.vue
@@ -0,0 +1,125 @@
+<template>
+  <Page>
+
+    // App buttons at the top (not bound yet)
+    <template v-slot:app-buttons>
+      <button id="reset" data-open="modal_new_board" class="reset button">
+        New Board
+      </button>
+      <button id="randomize" class="randomize button" title="Randomizes the player order. This option will disappear after picking has begun."
+      >
+        Shuffle Players
+      </button>
+      <button id="start_picking" class="start-draft button" title="Begin the drafting process.">
+        Start Draft
+      </button>
+    </template>
+
+    // Form list area
+    <template v-slot:player-form>
+      <form v-on:submit.prevent="onSubmit" class="player-add-form">
+        <input v-model="newPlayer" class="player-add" tabindex="1" type="text" placeholder="Add a new player">
+      </form>
+    </template>
+
+    // List of players, will clean up much more soon
+    <template v-slot:players-list>
+      <!-- Temp -->
+      <div
+        v-for="player in players"
+        :key="player.name"
+        v-on:click="activePlayer=player.name"
+        class="cell small-2 medium-3 large-auto"
+      >
+        <div class="card player" :class="{'player--owned': ownedPlayer === player.name, 'player--current': activePlayer === player.name}">
+          <div class="card-section">
+            <h4>{{ player.name}}</h4>
+
+            <div class="player-picker-outer">
+              <button
+                class="player-picker button expanded"
+                :class="{'hollow': ownedPlayer === player.name && activePlayer === player.name}"
+                v-on:click="ownedPlayer = player.name"
+              >
+                Be This Player
+              </button>
+            </div>
+
+            <div class="player-roster-container">
+              <div class="player-roster" :class="{'player-roster--empty': !player.characters.length}">
+                <div
+                  class="character"
+                  v-for="character in player.characters"
+                  :key="`player-character-${character.name}`"
+                  :style="{backgroundImage: `url(${character.image})`}"
+                >
+                  <span class="character-name">{{character.name}}</span>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+
+    // Full characters roster
+    <template v-slot:characters>
+      <div
+        class="character"
+        v-for="char in allCharacters"
+        :key="`character-${char.name}`"
+        v-on:click="addCharToPlayer(char.name)"
+      >
+        <img :src="char.image"/>
+      </div>
+    </template>
+
+  </Page>
+</template>
+
+<script>
+import Page from './Page';
+import allCharacters from '../../lib/chars';
+
+export default {
+  name: 'app',
+  methods: {
+    onSubmit() {
+      // Add new player
+      this.players.push({ name: this.newPlayer, characters: [], owned: false });
+      // Wipe input field
+      this.newPlayer = '';
+    },
+    addCharToPlayer(charName) {
+      // Find char object
+      const char = this.allCharacters.find(char => char.name === charName);
+      // Add it to end of active player's characters array
+      this.players.find(player => player.name === this.activePlayer).characters.push(char);
+      // Remove char from full roster
+      this.allCharacters = this.allCharacters.filter(char => char.name !== charName);
+    },
+  },
+  data() {
+    return {
+      newPlayer: '',
+      activePlayer: '',
+      ownedPlayer: '',
+      allCharacters: allCharacters.chars,
+      players: [],
+    };
+  },
+  components: {
+    Page
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'assets/scss/settings';
+  @import 'assets/scss/system';
+  @import 'assets/scss/board';
+  @import 'assets/scss/characters';
+  @import 'assets/scss/players';
+  @import '~foundation-sites/scss/foundation';
+</style>
---
+++ vue-smash/src/Page.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="grid-container">
+    <div class="grid-x grid-margin-x">
+      <div class="cell medium-6 large-4">
+        <h1>SmashCrowd</h1>
+        <p>Draft your next online Smashdown!</p>
+      </div>
+
+      <div class="cell medium-6 large-8 text-right">
+
+        <div id="board_info_container" class="board-info">
+          <h4>Draft Type: snake</h4>
+          <h4>Round: 0</h4>
+          <div class="board-options grid-x grid-margin-x">
+            <div class="cell auto">
+
+              <slot name="app-buttons"></slot>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="players_container">
+      <div class="grid-x grid-margin-x">
+        <div class="medium-6 cell">
+
+          <slot name="player-form"></slot>
+
+        </div>
+      </div>
+
+      <div class="grid-x grid-margin-x">
+
+        <slot name="players-list"></slot>
+
+      </div>
+    </div>
+
+    <div id="characters-container" class="grid-x grid-margin-x">
+      <div class="cell">
+        <div class="character-grid">
+
+          <slot name="characters"></slot>
+
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
